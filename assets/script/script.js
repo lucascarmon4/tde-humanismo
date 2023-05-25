a0856d42c5cf514845c873a4a5f02a4618dc5c8d
Lucas Carmona <lucascarmonaneto510@gmail.com>
2023-05-25 12:03:04 -0300

    testing css fonts ghpages

---
+++ assets/script/script.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal({ reset:true });
+
+sr.reveal('.a', { 
+    distance: '50px',
+    scale: 1.2,
+    duration: 1000,
+    interval: 200,
+    origin: 'top'
+});
+
+sr.reveal('#imgi', { 
+    scale: 0.9,
+    distance: '50px',
+    duration: 800,
+    origin: 'right'
+});
+sr.reveal('.b', { 
+    distance: '50px',
+    scale: 0.9,
+    duration: 1000,
+    interval: 200,
+    origin: 'left'
+});
+sr.reveal('.c', { 
+    distance: '20px',
+    scale: 0.9,
+    duration: 1000,
+    interval: 200,
+    origin: 'right'
+});
